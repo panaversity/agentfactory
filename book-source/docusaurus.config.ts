4ee124a488c308a3a96617d1ea9fd4c51579ffbc
Abdul Hannan <shaikhhannan729@gmail.com>
2025-11-15 01:19:30 +0500

    base url added

--- book-source/docusaurus.config.ts
+++ book-source/docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://ai-native.panaversity.org",
+  baseUrl: "/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
 
